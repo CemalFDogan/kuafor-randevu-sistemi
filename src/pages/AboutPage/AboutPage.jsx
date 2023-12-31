3524929a79d51e842c978df849f371449620f514
halilibrahimkartal <halilibrahimmkartal@gmail.com>
2023-12-31 17:46:37 +0300

    Sayfalar düzenlendi.

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,40 +1,45 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import hakkimizdaİlkResim from "../../assets/images/hakkimizda_image/1.jpeg"
+import hakkimizdaİkinciResim from "../../assets/images/hakkimizda_image/2.jpg"
+import hakkimizdaUcuncuResim from "../../assets/images/hakkimizda_image/3.jpg"
+
 
 export default function AboutPage() {
+  
   return (
-    <Container style={{ marginTop: '2rem' }}>
-      <Row className="justify-content-md-center">
-        <Col md={8}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Hakkımızda</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Biz Kimiz?</Card.Subtitle>
-              <Card.Text>
-                MyApp, modern ve kullanıcı dostu web çözümleri sunan öncü bir teknoloji şirketidir.
-                Misyonumuz, en yenilikçi ve etkili yazılım çözümlerini sunarak kullanıcılarımızın 
-                hayatını kolaylaştırmaktır.
-              </Card.Text>
-              <Card.Text>
-                Etkileyici bir kullanıcı deneyimi, yüksek performans ve erişilebilirlik, bizim için en
-                önemli önceliklerdir. Uzman ekibimiz, her bir proje için özel olarak tasarlanmış 
-                çözümler üretmektedir.
-              </Card.Text>
-              <Card.Title>Vizyonumuz</Card.Title>
-              <Card.Text>
-                Teknolojinin sürekli değişen dünyasında, yenilikçi ve sürdürülebilir çözümler 
-                sunarak sektörde lider olmayı hedefliyoruz. Kullanıcılarımızın ve müşterilerimizin 
-                memnuniyeti, bu yoldaki en büyük motivasyon kaynağımızdır.
-              </Card.Text>
-              <Card.Title>Neden Biz?</Card.Title>
-              <Card.Text>
-                Uzman ekibimiz, yılların getirdiği tecrübe ve bilgi birikimi ile her türlü zorluğun 
-                üstesinden gelmeye hazırdır. İnovatif yaklaşımlar ve son teknoloji çözümler ile 
-                sizlere hizmet vermekteyiz.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className="hakkimizda-container">
+    {/* İlk İçerik */}
+    <div className="hakkimizda-content">
+      <div className="image-container">
+        <img src={hakkimizdaİlkResim} alt="İşletme Resmi 1" className="img-fluid" />
+      </div>
+      <div className="text-container">
+        <h2>Hakkımızda 1</h2>
+        <p>İşletmeniz hakkında bilgi buraya gelecek.</p>
+      </div>
+    </div>
+
+    {/* İkinci İçerik */}
+    <div className="hakkimizda-content">
+    <div className="text-container">
+        <h2>Hakkımızda 2</h2>
+        <p>İşletmeniz hakkında bilgi buraya gelecek.</p>
+      </div>
+      <div className="image-container">
+        <img src={hakkimizdaİkinciResim} alt="İşletme Resmi 2" className="img-fluid" />
+      </div>
+      
+    </div>
+
+    {/* Üçüncü İçerik */}
+    <div className="hakkimizda-content">
+      <div className="image-container">
+        <img src={hakkimizdaUcuncuResim} alt="İşletme Resmi 3" className="img-fluid" />
+      </div>
+      <div className="text-container">
+        <h2>Hakkımızda 3</h2>
+        <p>İşletmeniz hakkında bilgi buraya gelecek.</p>
+      </div>
+    </div>
+  </div>
   );
 }
--- src/pages/SalonPage/SalonPage.jsx
+++ src/pages/SalonPage/SalonPage.jsx
@@ -1,3 +1,35 @@
+
+import salonA from "../../assets/images/salon_image/1.jpeg"
+import salonB from "../../assets/images/salon_image/2.jpeg"
+import salonC from "../../assets/images/salon_image/3.jpeg"
+import salonD from "../../assets/images/salon_image/4.jpeg"
+import salonE from "../../assets/images/salon_image/5.jpeg"
+import salonF from "../../assets/images/salon_image/6.jpeg"
+
+
 export default function SalonPage() {
-    return <h1>Salon Sayfası</h1>;
+    return (
+        <div className="container my-5">
+        <div className="row">
+          <div className="col-md-4 mb-4">
+            <img src={salonA} alt="Gallery 1" className="img-fluid gallery-image" />
+          </div>
+          <div className="col-md-4 mb-4">
+            <img src={salonB} alt="Gallery 2" className="img-fluid gallery-image" />
+          </div>
+          <div className="col-md-4 mb-4">
+            <img src={salonC} alt="Gallery 3" className="img-fluid gallery-image" />
+          </div>
+          <div className="col-md-4 mb-4">
+            <img src={salonD} alt="Gallery 4" className="img-fluid gallery-image" />
+          </div>
+          <div className="col-md-4 mb-4">
+            <img src={salonE} alt="Gallery 5" className="img-fluid gallery-image" />
+          </div>
+          <div className="col-md-4 mb-4">
+            <img src={salonF} alt="Gallery 6" className="img-fluid gallery-image" />
+          </div>
+        </div>
+      </div>
+    );
 }
