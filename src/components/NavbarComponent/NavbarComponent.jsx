bde0f4be5adf216d09b28888c07eefe9adf50c3c
CemalFDogan <cxexmxaxl4409@gmail.com>
2023-12-31 17:32:19 +0300

    Hairdresser Navbarı oluşturuldu.

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,15 +1,26 @@
 import AdminNavbar from './AdminNavbar';
 import UserNavbar from './UserNavbar';
 import DefaultNavbar from './DefaultNavbar';
+import HairstylistNavbar from './HairstylistNavbar';
 
 export default function NavbarComponent({ isLoggedIn, userRole }) {
-    console.log('isLoggedIn:', isLoggedIn, 'userRole:', userRole); // Bu satırı ekleyin
+    console.log('isLoggedIn:', isLoggedIn, 'userRole:', userRole); // Durum bilgisini konsola yazdır
 
     if (!isLoggedIn) {
-        // Kullanıcı giriş yapmamışsa, bu kısım çalışacak.
-        return <DefaultNavbar />; // Varsayılan Navbar componentiniz buraya gelebilir.
+        // Kullanıcı giriş yapmamışsa, varsayılan Navbar'ı döndür
+        return <DefaultNavbar />;
     }
 
-    return userRole === 'admin' ? <AdminNavbar /> : <UserNavbar />;
-
+    // Kullanıcı giriş yapmışsa, rolüne göre uygun Navbar'ı döndür
+    switch(userRole) {
+        case 'admin':
+            return <AdminNavbar />;
+        case 'user':
+            return <UserNavbar />;
+        case 'hairstylist':
+            return <HairstylistNavbar />;
+        default:
+            // Tanımsız bir rol varsa veya rol belirtilmemişse, varsayılan Navbar'ı döndür
+            return <DefaultNavbar />;
+    }
 };
